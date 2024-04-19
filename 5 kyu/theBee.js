e3f0b8cedff3153f22648b9c0e6f80f82766102d
edine-noella <edinenoella@gmail.com>
2024-04-19 18:07:42 +0200

    theBee

---
+++ 5 kyu/theBee.js
@@ -0,0 +1,13 @@
+//must identify all possible paths the bee can take from point A to point B
+//the bee can move south,north and west
+//the N is the like the number of cirlce grid steps from inside to outside for the hexagon
+//the bee will always travel N-1 steps because we are not counting the starting step
+//the bee moves in three directions (south,north and west) so it can take 3* (N-1) moves
+//but there is still alot of possible paths that the bee can take from those three mvoes depending on N
+//this is where binomial coefficient comes in
+
+
+
+function beePaths(N) {
+    
+}
