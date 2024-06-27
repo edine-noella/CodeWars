b2ae0e1a7b4aacba776a80949bc8a9a6506da565
edine-noella <edinenoella@gmail.com>
2024-06-27 11:40:25 +0200

    ch: greedIsGood

--- 5 kyu/greedIsGood.js
+++ 5 kyu/greedIsGood.js
@@ -11,30 +11,66 @@
 // One 1 => 100 points
 // One 5 => 50 point
 
-function score( dice ) {
+function score(dice) {
+  let score = 0;
+  let one1 = 100;
+  let one5 = 50;
   
-    let score = 0;
-    let count = 0;
+  for (let i = 0; i < dice.length; i++) {
     
-    let one1 = 100;
-    let one5 = 50;
-    
-    for (let i = 0 ; i < dice.length ; i++){
-       
-      if( (dice.filter( v => v === i).length) == 1 ){
-           
-        if( i == 1){
-          score += 100;        
-        }
-        
-        if( i == 5){
-          score += 50;
-        }
-         
-        
-      }else if ((dice.filter( v => v === i).length) == 3 ){       
-            count += Points;
-         }
+
+    if (dice.filter( (v) => v === i).length === 1) {
+      
+      if (dice[i] === 1) {
+        score += one1;
+      } 
+
+      if (dice[i] === 5) {
+        score += one5;
+      }
+      
     }
+    
+    if (dice.filter((v) => v === i).length === 2) {
+      if (dice[i] === 1) {
+        score += one1 * 2;
+      }
+
+      if (dice[i] === 5) {
+        score += one5 * 2;
+      }
+    }
+
+    if (dice.filter((v) => v === i).length === 3) {
+      if ( dice[i] === 1) {
+        score += 1000;
+      }
+
+      if (dice[i] === 6) {
+        score += 600;
+      }
+
+      if (dice[i] === 5) {
+        score += 500;
+      }
+
+      if (dice[i] === 4) {
+        score += 400;
+      }
+
+      if (dice[i] === 3) {
+        score += 300;
+      }
+
+      if (dice[i] === 2) {
+        score += 200;
+      }
+    }
+    
       
   }
+
+  return score;
+}
+
+console.log(score([2, 4, 4, 5, 4])); 
