19e4ffb978b5ea601116943bacec86939eb5ae3c
edine-noella <edinenoella@gmail.com>
2024-07-03 15:02:06 +0200

    englishBeggers

---
+++ 6 kyu/englishBeggers.js
@@ -0,0 +1,15 @@
+
+
+function beggers(values , n){
+
+    let results = Array(values.length).fill(0);
+    //fill  method fills specified elements in an array with a value
+
+    for ( let i = 0; i< values.length; i++ ){
+
+     
+    }
+
+}
+
+console.log(beggers([1,2,3,4,5],2));
