cb0bbd2bbb80942054e2d1f9a3ab6e4e5f71ced1
edine-noella <edinenoella@gmail.com>
2024-07-23 15:51:49 +0200

    commited

---
+++ 6 kyu/arrDiff.js
@@ -0,0 +1,4 @@
+
+function arrayDiff(a, b) {
+  return a.filter((el) => !b.includes(el));
+}
---
+++ 6 kyu/stopgninnipSMysdroW.js
@@ -0,0 +1,5 @@
+// 
+
+function spinWords(string){
+  return string.split(' ').map(word => word.length >= 5 ? word.split('').reverse().join('') : word).join(' ');
+}
