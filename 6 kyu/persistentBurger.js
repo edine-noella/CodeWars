dc7abdcead32dcefed09c517fbcabfcfb7cdcca4
edine-noella <edinenoella@gmail.com>
2024-08-01 16:47:35 +0200

    to comel case, presistend

---
+++ 6 kyu/persistentBurger.js
@@ -0,0 +1,10 @@
+
+
+function persistence(num) {
+    let count = 0;
+    while (num.toString().length > 1) {
+        num = num.toString().split('').reduce((a, b) => a * b);
+        count++;
+    }
+    return count;
+}
---
+++ 6 kyu/toCamelCase.js
@@ -0,0 +1,12 @@
+function toCamelCase(str){
+    let arr = str.split('-')
+    let result = arr[0][0].toLowerCase() + arr[0].slice(1);
+   
+    for (let i = 1 ; i< arr.length ; i++){
+        result += arr[i][0].toUpperCase() + arr[i].slice(1)
+    }
+    
+    return result;
+}
+
+console.log(toCamelCase('the-stealth-warrior')) // theStealthWarrior
